6c300c1a4704087917bd35c5ee8d94216a3cc8e4
bowsy <gautierlalanne64.2@gmail.com>
2025-12-10 14:36:58 +0100

    clientReady Event rename

---
+++ Events/clientReady.js
@@ -0,0 +1,26 @@
+const Discord = require('discord.js');
+const { ActivityType } = Discord;
+const loadSlashCommands = require("../Loaders/loadSlashCommands");
+const { startWebServer } = require("../Web/webServer");
+const express = require('express');
+
+
+module.exports = async client => {
+    try {
+        await loadSlashCommands(client);
+
+        console.log(`Le bot est en ligne ! ID: ${client.user.tag}`);
+        startWebServer(client);
+
+        // Notification initiale
+        const channelId = '1430581638856708288';
+        const channel = await client.channels.fetch(channelId).catch(console.error);
+        if (channel) {
+            await channel.send('Le bot est en ligne 🚀 !');
+            await channel.send('https://tenor.com/view/waddle-waddle-bitcoin-runes-pengu-waddle-waddle-pengu-gif-14234498873207878683');
+        }
+
+    } catch (error) {
+        console.error('Erreur dans l\'événement ready:', error);
+    }
+};
